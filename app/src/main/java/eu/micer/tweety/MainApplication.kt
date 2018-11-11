5b35184a8f7f38f0ed97befe9c2379b005510d28
Michal Cerman <michalcerman@gmail.com>
2018-11-11 22:11:34 +0100

    Added Leak Canary library. Added comments to libraries. Code refactoring.

--- app/src/main/java/eu/micer/tweety/MainApplication.kt
+++ app/src/main/java/eu/micer/tweety/MainApplication.kt
@@ -3,6 +3,7 @@ package eu.micer.tweety
 import android.support.multidex.MultiDexApplication
 import com.github.ajalt.timberkt.Timber
 import com.github.ajalt.timberkt.Timber.DebugTree
+import com.squareup.leakcanary.LeakCanary
 import eu.micer.tweety.di.allModules
 import eu.micer.tweety.util.UserPreference
 import org.koin.android.ext.android.startKoin
@@ -10,6 +11,12 @@ import org.koin.android.ext.android.startKoin
 class MainApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
         Timber.plant(DebugTree())
 
